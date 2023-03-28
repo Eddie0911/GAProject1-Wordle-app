12ef2faabe8c0412d00b248d96484d3ea91e716b
eddiew <eddieforwork0911@gmail.com>
2023-03-28 21:39:53 +1100

    Eddie GA project1

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# GAProject1-Wordle-app
-GA Unit1 Project1 Wordle-app
+# **Project title: GAProject1-Wordle-app**
+
+## Project Description ##
+**Wordle-app** is a popular online word game where players are given a five-letter random word and must guess the word within a limited number of attempts. The objective of the game is to guess the word correctly by entering possible words into the game interface. Each attempt is marked with colored squares to indicate whether a letter is in the correct position or not. A green square indicates that the letter is in the correct position, and a yellow square indicates that the letter is in the word but in the wrong position. The game is won when the player correctly guesses the word within the given attempts, and it becomes progressively more challenging as the player advances to higher levels. Wordle is a simple yet addictive game that tests players' vocabulary skills and strategic thinking.
+
+
+## How to install and Run the project ##
+Wordle is an online game that can be played directly on the Wordle website without the need for installation. To play Wordle, follow these steps:
+
+- Open a web browser and go to the Wordle website (https://www.powerlanguagegames.com/wordle/).
+
+- Once the Wordle website loads, You will see a talbe of 30squares and keyboard, you need to guess a five-letter random word by using the keyboard, 5 letters each time, after you done. click the *"Enter"* button.
+
+- The game will display colored squares to indicate whether your guess is correct or not. Green squares indicate that a letter is in the correct position, while yellow squares indicate that a letter is in the word but in the wrong position.
+
+- Continue guessing until you correctly guess the word or run out of attempts.
+
+- If you correctly guess the word within the given attempts, you win the game.
+
+- To play again, click the *"Restart"* button on the top-right of your website.
+
+- For more information about the game, click the *?* button o nthe top-left of your website.
+
+![How to play.](howtoplay.jpg)
---
+++ wordle.js
@@ -0,0 +1,159 @@
+function howToPlay(){
+    let image = document.getElementById('howtoplay');
+    image.sc="howtoplay.jpgr";
+    alert("Onload image"+image);
+}
+
+function refreshPage(){
+    window.location.reload();
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    createSquares();
+
+    //Function for making a square box for the letters//
+    function createSquares(){
+        //Make 30 divs and add class names square/animate__animated(for flipInX) and id from 1 to 30//
+        for(let index = 0;index <30; index++){
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.classList.add('animate__animated');
+            square.setAttribute('id',index+1);
+            document.getElementById('board').appendChild(square);
+        }
+    }
+
+    //Get the one ramdom 5letters word from the wordle-words.js and set them to lowercase to match the keyboard//
+    function getRandomElement(array) {
+        const randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+      }
+    const newWord = getRandomElement(validWords);
+    let word = newWord.toLowerCase();
+    console.log(word);      
+
+    
+    //Create a array to save the keys//
+    let guessWord = [[]];
+    // Set for catch ID//
+    let aviableSpace = 1;
+    //Set for helping finding the id of the first letters for each guss//
+    let guessWordCount = 0;
+
+
+//Get the key value from click//
+    const keys = document.querySelectorAll(".key");
+    for(let i = 0; i < keys.length; i++){
+        keys[i].onclick = (event) =>{
+        const key = event.target.getAttribute('data-key');
+        //If key = enter , click the Enter button tp call the submit function//
+        if(key === 'enter'){
+            submitWord()
+            return;
+        }
+        //If key = delete , click the delete dutton to call the deleteLetter function//
+        if(key === 'delete'){
+            deleteLetter();
+            return;
+        }
+        updateGuessedWords(key);
+    }
+}
+
+//change color
+    function getTileColor(key,index){
+        const correctLetter = word.includes(key)
+        
+        if (!correctLetter){
+            return "rgb(58,58,60)";
+        }
+
+        const rightPosition = word.charAt(index)
+        console.log(rightPosition);
+        const correctPosition = (key === rightPosition);
+        console.log(correctPosition)
+
+        if(correctPosition){
+            return "rgb(83,141,78)";
+        }
+
+        return "rgb(181,159,59)";
+    }
+//function for delete last letter//
+    function deleteLetter(){
+        const currentWordArray = getCurrentWordArray();
+        const remove = currentWordArray.pop();
+
+        guessWord[guessWord.length - 1] = currentWordArray;
+
+        const lastLetter = document.getElementById(`${aviableSpace -1}`);
+        lastLetter.textContent = '';
+        aviableSpace = aviableSpace - 1;
+    }
+
+//function for submit words//
+    function submitWord(){
+        const currentWordArray = getCurrentWordArray();
+        if(currentWordArray.length !== 5 ){
+            window.alert('must be 5 letters');
+            return updateGuessedWords;
+        }
+
+        const currentWord = currentWordArray.join(''); 
+        const firstLetterId = guessWordCount*5 +1;
+        const interval = 200;
+
+        currentWordArray.forEach((key,index)=>{
+            setTimeout(()=>{
+                const tileColor = getTileColor(key,index);
+                // 
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId);
+                letterEl.classList.add('animate__flipInX');
+                letterEl.style = `background-color:${tileColor}; border-color:${tileColor}`
+                
+            },interval*index)
+        })
+
+        guessWordCount += 1;
+
+        //If the guessWord matchs, alert message"Correct!".//
+        if(currentWord === word){
+            window.alert('Correct!')
+        }
+        
+        //If guess more than 6 times and still not give the answer alert message and show the correct word//
+        if(guessWord.length === 6){
+            window.alert(`Sorry,you have no more guesses`);
+            window.alert(`The word is ${word}.`);
+        }
+
+        guessWord.push([]);
+    }
+
+//Set a array to save the letters put//
+    function getCurrentWordArray(){
+        const numberOfGuessedWords = guessWord.length
+        return guessWord[numberOfGuessedWords - 1]
+    }
+
+//
+    function updateGuessedWords(key){
+        const currentWordArray = getCurrentWordArray()
+        //If the words the array is less than 5 keep update letters until 5letters//
+        if(currentWordArray && currentWordArray.length < 5){
+            currentWordArray.push(key)
+
+            //Put the data-key value to Id//
+            const aviableSpaceElement = document.getElementById(`${aviableSpace}`);
+            aviableSpace = aviableSpace + 1;
+            aviableSpaceElement.textContent = key;
+        }
+    }
+})
+
+
+
